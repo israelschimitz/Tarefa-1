93c1b5b3fa0bc514a5d5801549c1bb2538e3151b
israelschimitz <israelschimitz@hotmail.com>
2013-04-13 18:48:24 -0300

    V1 funcionando

--- tarefa1.c
+++ tarefa1.c
@@ -4,15 +4,14 @@ int vector [20];
 
 void main () {
 		
-	int i, j = 0; 
-	for (i=0:i<sizeof(vector):i++) {
-		scanf(%d,&vector[i]);
-
-	}
-        for (j=0:j<sizeof(vector):j++) {
-                printf(%d,&vector[j]);
-
+	int i=0;
+	int j = 0;
+	system("clear");
+	printf("Digite os vinte valores:\n");
+	for (i=0;i<20;i++) {
+		scanf("%d",&vector[i]);
         }
-
-
-
+        for (j=0;j<20;j++) {
+                printf("%d""\n",vector[j]);
+        }
+}
