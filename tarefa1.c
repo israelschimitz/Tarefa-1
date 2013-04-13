86e284fc81307cc7248ac6578716d9a1d79e15f2
israelschimitz <israelschimitz@hotmail.com>
2013-04-13 14:36:48 -0300

    inicial - lê e escreve

---
+++ tarefa1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int vector [20];
+
+void main () {
+		
+	int i, j = 0; 
+	for (i=0:i<sizeof(vector):i++) {
+		scanf(%d,&vector[i]);
+
+	}
+        for (j=0:j<sizeof(vector):j++) {
+                printf(%d,&vector[j]);
+
+        }
+
+
+
