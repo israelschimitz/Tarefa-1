e706541baa9eff6811095fc69f472ad9c7a5cbf0
israelschimitz <israelschimitz@hotmail.com>
2013-04-16 18:48:07 -0300

    media habilitada

--- tarefa1.c
+++ tarefa1.c
@@ -5,40 +5,25 @@ int i=0;
 int j=0;
 int aux=0;
 
-void main () {
-		
-	system("clear");
-	leitura_dados ();
-        
-	printf("Valores digitados:\n");
-	imprime_valores ();
-
-	ordena_crescente ();
-
-	printf("Valores em ordem crecente:\n");
-	imprime_valores ();
-
-}
-
 int imprime_valores () {
-	for (j=0;j<20;j++) {
+        for (j=0;j<20;j++) {
                 printf("%d""\n",vector[j]);
         }
         j=0;
-	return 0;
+        return 0;
 }
 
 int leitura_dados () {
-	printf("Digite os vinte valores:\n");
-	for (i=0;i<20;i++) {
+        printf("Digite os vinte valores:\n");
+        for (i=0;i<20;i++) {
                 scanf("%d",&vector[i]);
         }
         i=0;
-	return 0;
+        return 0;
 }
 
 int ordena_crescente () {
-	for (i=0;i<20;i++) {
+        for (i=0;i<20;i++) {
                 for (j=1;j<20;j++) {
                         if (vector[j] < vector[j-1]) {
                                 aux=vector[j];
@@ -47,6 +32,39 @@ int ordena_crescente () {
                         }
                 }
         }
-	return 0;
+        i=0;
+        j=0;
+        return 0;
 }
+
+float calcula_media (int x[]) {
+        int aux1=0;
+        float m=0;
+        for (i=0;i<20;i++) {
+                aux1 = aux1+x[i];
+        }
+        i=0;
+        m=aux1/20;
+        return (m);
+}
+
+void main () {
+        float media=0;
+        float dsvpd=0;
+
+        system("clear");
+        leitura_dados ();
+
+        printf("Valores digitados:\n");
+        imprime_valores ();
+
+        ordena_crescente ();
+
+        printf("Valores em ordem crecente:\n");
+        imprime_valores ();
+
+        media = calcula_media(vector);
+        printf("A média é: %f\n", media);
+}
+
 
