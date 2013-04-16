a83f6d86eb9a83202b623d7811e65c8e2947b833
israelschimitz <israelschimitz@hotmail.com>
2013-04-16 16:58:04 -0300

    inicial - lê e escreve

--- tarefa1.c
+++ tarefa1.c
@@ -1,39 +1,52 @@
 #include <stdio.h>
 
 int vector [20];
+int i=0;
+int j=0;
+int aux=0;
 
 void main () {
 		
-	int i=0;
-	int j=0;
-	int aux=0;
 	system("clear");
-	printf("Digite os vinte valores:\n");
-
-	for (i=0;i<20;i++) {
-		scanf("%d",&vector[i]);
-        }
-	i=0;
+	leitura_dados ();
         
 	printf("Valores digitados:\n");
-        for (j=0;j<20;j++) {
-		printf("%d""\n",vector[j]);
-        }
-	j=0;
-	for (i=0;i<20;i++) {
-		for (j=1;j<20;j++) {
-        		if (vector[j] < vector[j-1]) {
-				aux=vector[j];
-				vector[j]=vector[j-1];
-				vector[j-1]=aux;
-			}
-		}
-	}
-        printf("Valores em ordem crecente:\n");
-        for (j=0;j<20;j++) {
-                printf("%d""\n",vector[j]);
-        }
-        
-        j=0;
+	imprime_valores ();
+
+	ordena_crescente ();
+
+	printf("Valores em ordem crecente:\n");
+	imprime_valores ();
 
 }
+
+int imprime_valores () {
+	for (j=0;j<20;j++) {
+                printf("%d""\n",vector[j]);
+        }
+        j=0;
+	return 0;
+}
+
+int leitura_dados () {
+	printf("Digite os vinte valores:\n");
+	for (i=0;i<20;i++) {
+                scanf("%d",&vector[i]);
+        }
+        i=0;
+	return 0;
+}
+
+int ordena_crescente () {
+	for (i=0;i<20;i++) {
+                for (j=1;j<20;j++) {
+                        if (vector[j] < vector[j-1]) {
+                                aux=vector[j];
+                                vector[j]=vector[j-1];
+                                vector[j-1]=aux;
+                        }
+                }
+        }
+	return 0;
+}
+
